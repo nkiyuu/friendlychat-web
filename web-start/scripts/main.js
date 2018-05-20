0ab191e4f0312a815b0b74f231d5c81cee5bacf6
Yuki Nozaki <nq127main@gmail.com>
2018-05-20 11:19:17 +0900

    on going

--- web-start/scripts/main.js
+++ web-start/scripts/main.js
@@ -55,7 +55,12 @@ function FriendlyChat() {
 
 // Sets up shortcuts to Firebase features and initiate firebase auth.
 FriendlyChat.prototype.initFirebase = function() {
-  // TODO(DEVELOPER): Initialize Firebase.
+    // Shrotcuts ot Firebase SDK features.
+    this.auth = firebase.auth();
+    this.database = firebase.database();
+    this.storage = firebase.storage();
+    // Initiates Firebase auth and listen to auth state changes.
+    this.auth.onAuthStateChanged(this.onAuthStateChanged.bind(this));
 };
 
 // Loads chat messages history and listens for upcoming ones.
